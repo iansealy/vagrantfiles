05ce4f3e6f5dea78b9cbd7d91f41fc8d77e5eeb8
Ian Sealy <git@iansealy.com>
2020-02-06 09:30:50 +0000

    New cert.

--- camvpn/provision.sh
+++ camvpn/provision.sh
@@ -8,5 +8,5 @@ apt-get -q -y install cifs-utils openfortivpn ppp screen
 echo "host = mercury.medschl.cam.ac.uk
 port = 10443
 username = is460
-trusted-cert = 4ff1710840fa01088254d5955ae3a316dacf01c59e81ddf3f5a2214cfdc48ed9" \
+trusted-cert = b10fe4bd3be20aa44cb9f2ca7ff2605661ca09705f1c360353344eb42ae3f404" \
 > /etc/openfortivpn/config
