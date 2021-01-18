b4dd0f6a826030e9b1f4fcf464f297768e1f4f43
Ian Sealy <git@iansealy.com>
2021-01-18 14:27:21 +0000

    Upgrade RStudio.

--- rstudio/provision-root.sh
+++ rstudio/provision-root.sh
@@ -9,6 +9,6 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E298A3A825C0D6
 add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/"
 apt-get -q -y install r-base r-base-dev
 
-wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.3.1093-amd64.deb
-apt-get -q -y install ./rstudio-1.3.1093-amd64.deb
-rm rstudio-1.3.1093-amd64.deb
+wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.4.1103-amd64.deb
+apt-get -q -y install ./rstudio-1.4.1103-amd64.deb
+rm rstudio-1.4.1103-amd64.deb
