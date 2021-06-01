2406f4bfc12e32f820687fea70a246765352326b
Ian Sealy <git@iansealy.com>
2021-06-01 11:23:00 +0100

    New VPN: https://confluence.medschl.cam.ac.uk/display/FAQ/New+VPN+Client+settings+for+Biomedical+Campus

--- camvpn/provision.sh
+++ camvpn/provision.sh
@@ -5,7 +5,7 @@ apt-get -q -y upgrade
 
 apt-get -q -y install cifs-utils openfortivpn ppp
 
-echo "host = mercury.medschl.cam.ac.uk
+echo "host = vpn.medschl.cam.ac.uk
 port = 10443
 username = is460
 trusted-cert = b10fe4bd3be20aa44cb9f2ca7ff2605661ca09705f1c360353344eb42ae3f404" \
